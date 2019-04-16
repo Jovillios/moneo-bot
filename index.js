be693bea8244624d4314132015cee0c1b37f4b3b
Jules Decaestecker <38179298+Jovillios@users.noreply.github.com>
2019-04-16 22:48:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const botconfig = require("./botconfig.json")
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const fs = require("fs")
+const fs = require("fs");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
